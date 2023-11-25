48afcafae78fdb232a407b34a9b9987ab48a5383
Xingyu Bian <30872286+therealcyberlord@users.noreply.github.com>
2023-11-25 11:03:15 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <Profile
         name="Xingyu Bian"
-        description="Senior at UMass Amherst, majoring in Computer Science. Experienced in AI/ML and full-stack development. Passionate about leveraging machine learning, particularly large language models (LLMs), to tackle complex problems. I am committed to continuous learning and thrive on embracing new challenges "
+        description="Senior at UMass Amherst, majoring in Computer Science. Experienced in AI/ML and full-stack development. I am committed to continuous learning and thrive on embracing new challenges "
         img_path="https://media.licdn.com/dms/image/D4E03AQFCQkdaurjz6w/profile-displayphoto-shrink_800_800/0/1683933220891?e=1701907200&v=beta&t=Prr6vLWkFRyrdpOxLZVeH9eDlFdrCJ8S6hEKyPvqECc"
         role="Senior at UMass Amherst"
       />
