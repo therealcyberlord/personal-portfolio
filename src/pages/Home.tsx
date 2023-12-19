c49ca5a51cc6b716a915e242b1e18cc5cb0d6e4d
Xingyu Bian <30872286+therealcyberlord@users.noreply.github.com>
2023-12-19 12:29:53 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <Profile
         name="Xingyu Bian"
         description="Senior at UMass Amherst, majoring in Computer Science. Experienced in AI/ML and full-stack development. I am committed to continuous learning and thrive on embracing new challenges "
-        img_path="https://media.licdn.com/dms/image/D4E03AQFCQkdaurjz6w/profile-displayphoto-shrink_800_800/0/1683933220891?e=1701907200&v=beta&t=Prr6vLWkFRyrdpOxLZVeH9eDlFdrCJ8S6hEKyPvqECc"
+        img_path="https://media.licdn.com/dms/image/D4E03AQEf0h6LFfB84g/profile-displayphoto-shrink_800_800/0/1701894263253?e=1708560000&v=beta&t=petjxc06VZUOo-k2V2wRAmviw9MzLElmO0ua5qsZOuY"
         role="Senior at UMass Amherst"
       />
     </>
