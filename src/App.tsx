1df2f96ecf0ae3cdcf72d1eea421f8210b2caae6
Xingyu Bian <xingyubian@Xingyus-Laptop.local>
2024-06-03 15:15:13 -0400

    revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
@@ -8,13 +8,14 @@ import Navbar from "./components/Nav";
 function App() {
   return (
     <div className="w-screen h-screen">
-      <Router>
+      <Router basename={import.meta.env.DEV ? "/" : "/personal-portfolio"}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
+         
         </Routes>
       </Router>
     </div>
