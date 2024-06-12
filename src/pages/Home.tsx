9b746a42e82d848f080588a5ef4837bc1477951c
Xingyu Bian <xingyubian@Xingyus-Laptop.local>
2024-06-11 22:42:58 -0400

    uses local images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <Profile
         name="Xingyu Bian"
         description="Master's in Computer Science at UMass Amherst, currently doing ML research with the UMass BioNLP Group."
-        img_path="https://media.licdn.com/dms/image/D4E03AQEf0h6LFfB84g/profile-displayphoto-shrink_400_400/0/1701894263253?e=1723075200&v=beta&t=epD2Xysog7cc4ht46wQZouZoSo6kYqcbOP-9lGb7hNA"
+        img_path="/src/assets/profile.jpg"
         role="Student at UMass Amherst"
       />
     </>
