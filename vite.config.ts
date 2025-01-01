92a91453753533074c3e54692bbb079cebe349cb
Xingyu Bian <xingyubiancyberland@gmail.com>
2024-12-31 22:30:18 -0500

    change vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({  mode }) => {
-  let base = "/";
-
-  if (mode === "github") {
-    base = "/personal-portfolio/";
-  }
-
-  return {
-    plugins: [react()],
-    base,
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/", // Ensures scripts are served from the root
 });
